956ea84d3ecbbbb7cc0a0e3fa0704e10883fdbb7
John Mosquera <tradexy@gmail.com>
2019-09-05 19:44:34 +0100

    test reat - affine property

---
+++ precache-manifest.ce79a8ae11eca12cc3c07d95240e0ad9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "927275daa5c3d9d9387f11a084482246",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c8ff09c0261f7caab49",
+    "url": "/static/css/2.9e4b045f.chunk.css"
+  },
+  {
+    "revision": "a378ba6dd8940c7cbb91",
+    "url": "/static/css/main.9c766497.chunk.css"
+  },
+  {
+    "revision": "0c8ff09c0261f7caab49",
+    "url": "/static/js/2.8184debc.chunk.js"
+  },
+  {
+    "revision": "a378ba6dd8940c7cbb91",
+    "url": "/static/js/main.c87cd13e.chunk.js"
+  },
+  {
+    "revision": "29599b54d2d607acc901",
+    "url": "/static/js/runtime~main.15cf0d81.js"
+  }
+]);
